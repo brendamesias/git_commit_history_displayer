ff1612131022217e3faa8ccdffd01b0d8b5f5d61
Brenda Mesias <brendamesiasvillar@gmail.com>
2021-08-06 16:45:19 -0500

    adjust styles

--- src/pages/list_history_commit.js
+++ src/pages/list_history_commit.js
@@ -1,34 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import {Card, Button} from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 import getGitCommitHistory from '../services/index'
 
 const ListHistoryCommit = () => {
+
+    const parseDate = (date) => {
+        const newDate = new Date(date)
+        return newDate.toDateString()
+    }
 
     const [commits, setCommits] = useState([])
 
     useEffect(() => {
         getAllCommits();
-    }, []); 
+    }, []);
 
     const getAllCommits = async () => {
         const allCommits = await getGitCommitHistory()
-        setCommits(allCommits);
+        if (allCommits) {
+            setCommits(allCommits);
+        }
     }
 
     return (
-        <Card style={{textAlign: 'initial'}}>
-            <Card.Header>Featured</Card.Header>
-            <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            <Card.Footer className="text-muted">2 days ago</Card.Footer>
-        </Card>
-    )
+        <div className='container_cards'>
+            {commits.map(commit =>
+                <Card key={commit.sha} style={{ textAlign: 'initial', marginTop: '2em' }}>
+                    <Card.Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <span>
+                            Commiter: {commit.commit.author.name}
+                        </span>
+                        <Card.Img
+                            style={{ width: '40px', marginLeft: '1em' }}
+                            src={commit.author.avatar_url}
+                            alt="Card image" />
+                    </Card.Header>
+                    <Card.Body>
 
+                        <Card.Text>
+                            {commit.commit.message}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">
+                        {parseDate(commit.commit.author.date)}
+                    </Card.Footer>
+                </Card>
+            )}
+        </div>
+    )
 }
 
 
